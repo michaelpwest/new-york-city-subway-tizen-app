c00f066b3cff7b85fd753543e5ccbad279d71486
Michael West <michaelpwest@users.noreply.github.com>
2019-04-07 11:09:48 -0400

    - Added basic UI. - Added service status and arrival times views.

---
+++ src/components/Navigation.vue
@@ -0,0 +1,7 @@
+<template>
+	<div id="nav">
+		<router-link to="/service-status">Service Status</router-link> |
+		<router-link to="/">Home</router-link> |
+		<router-link to="/arrival-times">Arrival Times</router-link>
+	</div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import ServiceStatus from "./views/ServiceStatus.vue";
+import ArrivalTimes from "./views/ArrivalTimes.vue";
 
 Vue.use(Router);
 
@@ -12,12 +14,14 @@ export default new Router({
 			component: Home,
 		},
 		{
-			path: "/about",
-			name: "about",
-			// route level code-splitting
-			// this generates a separate chunk (about.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import(/* webpackChunkName: "about" */ "./views/About.vue"),
+			path: "/service-status",
+			name: "service-status",
+			component: ServiceStatus,
+		},
+		{
+			path: "/arrival-times",
+			name: "arrival-times",
+			component: ArrivalTimes,
 		},
 	],
 });
