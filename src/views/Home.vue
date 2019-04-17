857f0d4b0b1c22489dfcd99f32ad3d510c8097da
Michael West <michaelpwest@users.noreply.github.com>
2019-04-16 21:57:23 -0400

    - Added home component.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,18 @@
 		<header class="ui-header">
 			<h2 class="ui-title">Home</h2>
 		</header>
-		<section class="ui-content"></section>
+		<section class="ui-content">
+			<Home></Home>
+		</section>
 	</div>
 </template>
+
+<script>
+import Home from "@/components/Home.vue";
+
+export default {
+	components: {
+		Home,
+	},
+};
+</script>
